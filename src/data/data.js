c22cfda6829731d1f887949bda0d08320b957a59
Serkan Basdemir <basdemirs@yahoo.com>
2020-06-08 16:50:35 +0300

    Initial Commit

---
+++ src/data/data.js
@@ -0,0 +1,114 @@
+export const data = {
+  nodes: [
+    {
+      id: '1',
+      label: 'Label 1'
+    },
+    {
+      id: '2',
+      label: 'Label 2'
+    },
+    {
+      id: '3',
+      label: 'Label 3'
+    },
+    {
+      id: '4',
+      label: 'Label 4'
+    },
+    {
+      id: '5',
+      label: 'Label 5'
+    },
+    {
+      id: '6',
+      label: 'Label 6'
+    },
+    {
+      id: '7',
+      label: 'Label 7'
+    },
+    {
+      id: '8',
+      label: 'Label 8'
+    },
+    {
+      id: '9',
+      label: 'Label 9'
+    }
+  ],
+  edges: [
+    {
+      source: '1',
+      target: '2',
+      data: {
+        type: 'name1',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '1',
+      target: '3',
+      data: {
+        type: 'name2',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '2',
+      target: '5',
+      data: {
+        type: 'name1',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '5',
+      target: '6',
+      data: {
+        type: 'name2',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '3',
+      target: '4',
+      data: {
+        type: 'name3',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '4',
+      target: '7',
+      data: {
+        type: 'name2',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '1',
+      target: '8',
+      data: {
+        type: 'name2',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    },
+    {
+      source: '1',
+      target: '9',
+      data: {
+        type: 'name3',
+        amount: '100,000,000,00 $',
+        date: '2019-08-03'
+      }
+    }
+  ]
+};
